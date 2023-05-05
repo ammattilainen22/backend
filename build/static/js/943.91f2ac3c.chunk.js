b8bc0db303cbe858f575e86093c63287e70cf066
ammattilainen22 <105714081+ammattilainen22@users.noreply.github.com>
2023-05-05 12:12:12 +0300

    Add files via upload

---
+++ build/static/js/943.91f2ac3c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdiary=self.webpackChunkdiary||[]).push([[943],{943:function(e,t,a){a.r(t);a(791),a(508);var s=a(184);t.default=function(){return(0,s.jsxs)("div",{className:"text-center",children:[(0,s.jsx)("h1",{className:"main-title home-page-title",children:"welcome to our app"}),(0,s.jsx)("button",{className:"primary-button",children:"Log out"})]})}}}]);
+//# sourceMappingURL=943.91f2ac3c.chunk.js.map
